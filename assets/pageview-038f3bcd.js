3656e57243a75763f9796279d4f4987a966e1c85
agisnrynt <agisnuryanto@outlook.co.id>
2023-02-15 09:34:53 +0000

    Deploying to gh-pages from @ agisnrynt/agisnrynt.github.io@9c0149b47c77bc3fb4d79a031a58fe0c2749c0bf 🚀

---
+++ assets/pageview-038f3bcd.js
@@ -0,0 +1,1 @@
+import{k as e}from"./app-8fd9064b.js";import"./framework-85e73530.js";const t=()=>e({serverURL:"https://waline-olwj29lg4-agisnrynt.vercel.app/"});export{t as updatePageview};
---
+++ assets/privacy.html-d225dd51.js
@@ -0,0 +1,1 @@
+import{_ as e,X as c,Y as r}from"./framework-85e73530.js";const t={};function _(a,n){return c(),r("div")}const s=e(t,[["render",_],["__file","privacy.html.vue"]]);export{s as default};
